e3200e39061782a70a5328546904ce8fde1ea62f
NhatNam <namnhat1110@gmail.com>
2021-04-19 14:31:53 +0700

    innitial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+
+const Square = (props) => {
+  console.log({ props })
+  return <button className="square" onClick={props.onSquareClicked(props.value)}>{props.hi}</button>;
+};
+
+
+const Board = (props) => {
+  const [xturn, setXturn] = useState(true)
+  const renderSquare = (i) => {
+    return (
+      <Square
+        value={i}
+        hi={props.squares[i]}
+        bye={i}
+        onSquareClicked={onSquareClicked}
+      />
+    );
+  };
+
+
+  const onSquareClicked = (i) => {
+    setXturn(!xturn)
+
+  }
+
+  const status = xturn ? "Next player: X" : "Next Player : O"
+
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+
+const Game = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  console.log({ squares })
+
+  return (
+    <div className="game">
+      <div className="d-flex flex-column">
+        <h1> Tic tac toe </h1>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+        </ul>
+      </div>
+
+      <div className="game-board">
+        <Board squares={squares} setSquares={setSquares} />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  )
+}
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game />
     </div>
   );
 }
